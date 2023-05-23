8a00647f9095bff8409c58e5b55f002af58e847e
Rangan Das <rangan@live.in>
2023-05-23 13:53:47 +0530

    Update filter_results.py

--- filter_results.py
+++ filter_results.py
@@ -1,6 +1,7 @@
 #%%
 import pandas as pd
 df = pd.read_csv('edges.tsv', sep='\t')
+df.drop_duplicates(inplace=True)
 # %%
 df[['src', 'dst', 'detail',]].to_csv('filtered_edges.tsv', sep='\t', index=False)
 # %%
